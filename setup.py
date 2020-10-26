c6536af58338e171e90ce0fe5ba074a8fd274cfe
EMVGaron <eric.march91@gmail.com>
2020-10-26 16:59:16 +0100

    changed installation settings

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
-   name='itex_codes',
+   name='itex_codes_refactorized',
    version='1.0',
-   description='Codes to create and mantain CII db',
+   description='Codes to mantain CII refactorized version',
    license='GNU',
    long_description=long_description,
-   author='Eric March Vila',
-   author_email='eric.march@upf.edu',
-   url='https://github.com/phi-grib/Itex_codes',
-   packages=['CreateDB','UpdateDB']
+   author='Eric March Vila, Giacomo Ferretti',
+   author_email='eric.march@upf.edu','gferretti85@gmail.com',
+   url='https://github.com/phi-grib/Itex_codes_refactorized',
+   packages=['UpdateDB']
 )
